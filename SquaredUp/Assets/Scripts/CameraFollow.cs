f1e038add02907fed1fe72e9725e9273ea295d4b
SenalikW0106 <senalikw0106@my.uwstout.edu>
2021-02-10 18:40:49 -0600

    Interaction

---
+++ SquaredUp/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField]
+    private Transform followTarget = null;
+
+    // LateUpdate is called once per end of frame
+    void LateUpdate()
+    {
+        Vector3 pos = followTarget.position;
+        pos.y = this.transform.position.y;
+        this.transform.position = pos;
+    }
+}
---
+++ SquaredUp/Assets/Scripts/PlayerInteract.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInteract : MonoBehaviour
+{
+    // The interactable the player is currently focused on.
+    private Interactable currentInteract;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentInteract = null;
+    }
+
+    // Called when an object enters the trigger.
+    private void OnTriggerEnter(Collider other)
+    {
+        currentInteract = other.GetComponent<Interactable>();
+    }
+
+    // Called when an object leaves the trigger.
+    private void OnTriggerExit(Collider other)
+    {
+        Interactable leaveInteract = other.GetComponent<Interactable>();
+        if (currentInteract == leaveInteract)
+        {
+            currentInteract = null;
+        }
+    }
+
+    // Called when an object stays in the trigger.
+    private void OnTriggerStay(Collider other)
+    {
+        if (currentInteract == null)
+        {
+            currentInteract = other.GetComponent<Interactable>();
+        }
+    }
+
+    // Called when the player tries to interact with something from the input system.
+    public void OnInteract(InputAction.CallbackContext value)
+    {
+        if (currentInteract != null && value.started)
+        {
+            currentInteract.Interact();
+        }
+    }
+}
