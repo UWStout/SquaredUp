335f51e0f80460f973462dddb418ee39ef4f49a6
SenalikW0106 <wsenalik@hotmail.com>
2021-03-17 17:21:24 -0500

    Changed Rectangle Unlock Interactable in my Scene

---
+++ SquaredUp/Assets/Scripts/Rotate.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+/// <summary>Rotates the object its attached to</summary>
+public class Rotate : MonoBehaviour
+{
+    // Speed of rotation
+    [SerializeField] private Vector3 rotationSpeed = Vector3.one;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        transform.Rotate(rotationSpeed * Time.deltaTime);
+    }
+}
