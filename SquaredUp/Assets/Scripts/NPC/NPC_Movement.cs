91768bed48d1a751feb7395460b5674868e6d394
iz3k93 <isaacmoua93@gmail.com>
2021-03-24 17:19:43 -0500

    Push Npc Movement

--- SquaredUp/Assets/Scripts/NPC/NPC_Movement.cs
+++ SquaredUp/Assets/Scripts/NPC/NPC_Movement.cs
@@ -5,19 +5,21 @@ using UnityEngine;
 public class NPC_Movement : MonoBehaviour
 {
 
-    public float movementSpeed;
-
     private Rigidbody2D NPCRigidBody;
 
     public bool isWalking;
 
-    public float walkingTime;
+    public float walkingSpeed;
     private float walkingCounter;
     public float stopTime;
     private float stopCounter;
 
     public int[] paths;
     private int[] route;
+
+    public float[] movementTime;
+    private float[] movementList;
+
     private int numSpot;
     private int position;
     private int newLength;
@@ -29,45 +31,21 @@ public class NPC_Movement : MonoBehaviour
         NPCRigidBody = GetComponent<Rigidbody2D>();
 
         stopCounter = stopTime;
-        walkingCounter = walkingTime;
+        walkingCounter = walkingSpeed;
         numSpot = 0;
+        
         newLength = paths.Length * 2;
         route = new int[newLength];
-        Debug.Log(route.Length);
+
+        newLength = movementTime.Length * 2;
+        movementList = new float[newLength];
+        //Debug.Log(route.Length);
+
+        ExpandPathList(paths, route);
+
+        ExpandMovementList(movementTime, movementList);
 
 
-        for (int i = 0;i < paths.Length; i++)
-        {
-            //Debug.Log();
-            route[numSpot] = paths[i];
-            numSpot++;
-        }
-
-        for (int j = paths.Length - 1;j >= 0; j--)
-        {
-            Debug.Log(numSpot);
-            switch (paths[j])
-            {
-                case 0:
-                    route[numSpot] = 2;
-                    break;
-                case 1:
-                    route[numSpot] = 3;
-                    break;
-                case 2:
-                    route[numSpot] = 0;
-                    break;
-                case 3:
-                    route[numSpot] = 1;
-                    break;
-            }
-
-            numSpot++;
-            
-        }
-        Debug.Log(numSpot);
-        numSpot = 0;
-        Debug.Log(numSpot);
     }
 
     // Update is called once per frame
@@ -77,20 +55,20 @@ public class NPC_Movement : MonoBehaviour
         if (isWalking)
         {
             walkingCounter -= Time.deltaTime;
-
             switch (walkDirection)
             {
                 case 0:
-                    NPCRigidBody.velocity = new Vector2(0, movementSpeed);
+
+                    NPCRigidBody.velocity = new Vector2(0, walkingSpeed);
                     break;
                 case 1:
-                    NPCRigidBody.velocity = new Vector2(movementSpeed, 0);
+                    NPCRigidBody.velocity = new Vector2(walkingSpeed, 0);
                     break;
                 case 2:
-                    NPCRigidBody.velocity = new Vector2(0, -movementSpeed);
+                    NPCRigidBody.velocity = new Vector2(0, -walkingSpeed);
                     break;
                 case 3:
-                    NPCRigidBody.velocity = new Vector2(-movementSpeed, 0);
+                    NPCRigidBody.velocity = new Vector2(-walkingSpeed, 0);
                     break;
             }
             if (walkingCounter < 0)
@@ -98,7 +76,7 @@ public class NPC_Movement : MonoBehaviour
                 isWalking = false;
                 stopCounter = stopTime;
                 numSpot++;
-                Debug.Log(numSpot);
+                //Debug.Log(numSpot);
             }
         }
         else
@@ -110,22 +88,84 @@ public class NPC_Movement : MonoBehaviour
 
             if (stopCounter < 0)
             {
-                if (numSpot <= route.Length - 1)
-                {
-                    walkDirection = route[numSpot];
-                    isWalking = true;
-                    walkingCounter = walkingTime;
-                } else
+                if (numSpot > route.Length - 1)
                 {
                     numSpot = 0;
-                    walkDirection = route[numSpot];
-                    isWalking = true;
-                    walkingCounter = walkingTime;
                 }
+                Debug.Log(numSpot);
+                walkDirection = route[numSpot];
+                isWalking = true;
+                walkingCounter = movementList[numSpot];
                 
             }
         }
 
     } 
+
+    private void ExpandPathList(int[] input, int[] output)
+    {
+        for (int i = 0; i < input.Length; i++)
+        {
+            output[numSpot] = input[i];
+            numSpot++;
+        }
+
+        for (int j = input.Length - 1; j >= 0; j--)
+        {
+            switch (paths[j])
+            {
+                case 0:
+                    output[numSpot] = 2;
+                    break;
+                case 1:
+                    output[numSpot] = 3;
+                    break;
+                case 2:
+                    output[numSpot] = 0;
+                    break;
+                case 3:
+                    output[numSpot] = 1;
+                    break;
+            }
+
+            numSpot++;
+
+        }
+        numSpot = 0;
+    }
+
+    private void ExpandMovementList(float[] input, float[] output)
+    {
+        for (int i = 0; i < input.Length; i++)
+        {
+           // Debug.Log(numSpot);
+            output[numSpot] = input[i];
+            numSpot++;
+        }
+
+        for (int j = input.Length - 1; j >= 0; j--)
+        {
+            switch (paths[j])
+            {
+                case 0:
+                    output[numSpot] = input[j];
+                    break;
+                case 1:
+                    output[numSpot] = input[j];
+                    break;
+                case 2:
+                    output[numSpot] = input[j];
+                    break;
+                case 3:
+                    output[numSpot] = input[j];
+                    break;
+            }
+
+            numSpot++;
+
+        }
+        numSpot = 0;
+    }
 }
+
 
