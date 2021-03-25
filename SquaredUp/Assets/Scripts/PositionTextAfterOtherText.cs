fdb15b3746756fda02fcda11632095985859b686
Wyatt Senalik <wsenalik@hotmail.com>
2021-03-24 22:22:05 -0500

    More prefab stuff

--- SquaredUp/Assets/Scripts/PositionTextAfterOtherText.cs
+++ SquaredUp/Assets/Scripts/PositionTextAfterOtherText.cs
@@ -1,32 +1,29 @@
 ﻿using UnityEngine;
 
+/// <summary>Places the after ui element directly to the right of the before ui element on start.</summary>
 public class PositionTextAfterOtherText : MonoBehaviour
 {
+    // Element to place the after ui element after
     [SerializeField] private RectTransform beforeUIElement = null;
+    // Element to position after the before ui element
     [SerializeField] private RectTransform afterUIElement = null;
+    // Offset for where the element should be place after the before element
     [SerializeField] private Vector3 offset = Vector3.zero;
 
     // Start is called before the first frame update
     private void Start()
     {
         // Get the center right of the before text
-        Vector2 beforeRectPos = beforeUIElement.rect.position;
-        Vector3 beforeCenter = beforeUIElement.position + new Vector3(beforeRectPos.x, 0);
-        float beforeRightX = beforeCenter.x + beforeUIElement.rect.width / 2;
+        Vector2 beforeRectCenter = beforeUIElement.rect.center;
+        Vector3 beforeCenter = beforeUIElement.position + new Vector3(beforeRectCenter.x, beforeRectCenter.y);
+        float beforeRightX = beforeCenter.x + beforeUIElement.offsetMax.x;
         Vector3 beforeCenterRight = new Vector3(beforeRightX, beforeCenter.y);
-        Debug.Log("BeforeCenterRight: " + Vector3DetailedToString(beforeCenterRight));
 
         // Get the position we should place the after text at
-        float afterHalfSizeX = beforeUIElement.rect.width / 2;
+        float afterHalfSizeX = -beforeUIElement.offsetMin.x;
         Vector3 afterPlacePos = beforeCenterRight + new Vector3(afterHalfSizeX, 0) + offset;
-        Debug.Log("AfterPlacePos: " + Vector3DetailedToString(afterPlacePos));
 
         // Place the text there
         afterUIElement.position = afterPlacePos;
-    }
-
-    private string Vector3DetailedToString(Vector3 vector)
-    {
-        return "(" + vector.x + ", " + vector.y + ", " + vector.z + ")";
     }
 }
