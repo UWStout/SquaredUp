6ad76061d6a25fee59324cff9680842936c9e2d1
iz3k93 <isaacmoua93@gmail.com>
2021-03-25 00:46:47 -0500

    removed the unneeded start update on Vision

--- SquaredUp/Assets/Scripts/NPC/VisionInRange.cs
+++ SquaredUp/Assets/Scripts/NPC/VisionInRange.cs
@@ -9,19 +9,6 @@ public class VisionInRange : MonoBehaviour
     public int prisonX;
     public int prisonY;
 
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log("enter");
