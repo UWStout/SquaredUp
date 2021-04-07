3219450d3d1a839a68a90a5642158884ba74edb0
FiniteWill <60437388+FiniteWill@users.noreply.github.com>
2021-04-07 17:43:40 -0500

    Work on Player Movement

---
+++ SquaredUp/Assets/PushableCollision.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushableCollision : MonoBehaviour
+{
+   
+    [SerializeField] float slideSpeed = 1f;
+    [SerializeField] private Rigidbody2D rb2d;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Pushable collision");
+        Vector3 contactPoint = collision.contacts[0].point;
+        Vector3 moveDirection = CastDirectionToCardinal(transform.position - contactPoint);
+        rb2d.velocity = (moveDirection * slideSpeed);
+    }
+    private Vector3 CastDirectionToCardinal(Vector3 direction)
+    {
+        float rightDifference = (direction - Vector3.right).sqrMagnitude;
+        float leftDifference = (direction - Vector3.left).sqrMagnitude;
+        float upDifference = (direction - Vector3.up).sqrMagnitude;
+        float downDifference = (direction - Vector3.down).sqrMagnitude;
+        if (rightDifference < leftDifference && rightDifference < upDifference && rightDifference < downDifference)
+        {
+            return Vector3.right;
+        }
+        else if (leftDifference < upDifference && leftDifference < downDifference)
+        {
+            return Vector3.left;
+        }
+        else if (upDifference < downDifference)
+        {
+            return Vector3.up;
+        }
+        else
+        {
+            return Vector3.down;
+        }
+    }
+}
