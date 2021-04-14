66f252c6f7d7a1f7f66c65013d5c405d5a260a6a
SenalikW0106 <wsenalik@hotmail.com>
2021-04-13 23:30:31 -0500

    Don't destory on load and trigger to load next level

---
+++ SquaredUp/Assets/Scripts/DontDestroy.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Calls dont destroy on load for the gameobject
+/// </summary>
+public class DontDestroy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
