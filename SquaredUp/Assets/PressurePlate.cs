679cddc8d16853db4af302c633c143793803b095
FiniteWill <60437388+FiniteWill@users.noreply.github.com>
2021-04-18 22:16:53 -0500

    Castle Laser (and ice) Puzzle
    Finally finished the laser puzzle in the castle. It felt a bit sparse so I added a ice block section with a simple pressure plate trigger object. It should be impossible to soft-lock.

---
+++ SquaredUp/Assets/PressurePlate.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePlate : MonoBehaviour
+{
+    [SerializeField] private GameObject wall; // obstacle being controlled by pressure plate
+    private bool isIn = false; // Boolean for checking if the player is in the trigger
+    public GameObject other;
+    // Start is called before
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Check if player is still inside the trigger, if not set isIn to true, stop exiting music track, play entering music track
+        if (!isIn)
+        {
+            wall.SetActive(false);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        // Set isIn to false so the trigger will reset
+        isIn = false;
+        wall.SetActive(true);
+    }
+}
