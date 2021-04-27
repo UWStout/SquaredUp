2a9200fbb40700e201fdf329e9462391afc278c1
SenalikW0106 <wsenalik@hotmail.com>
2021-04-27 17:58:39 -0500

    Teleport for skipping king's long hallway

---
+++ SquaredUp/Assets/Scripts/TeleportTrigger.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+/// <summary>Teleports whatever walks in it to the target location.</summary>
+public class TeleportTrigger : MonoBehaviour
+{
+    // The transform of the location to teleport to
+    [SerializeField] private Transform targetLocation = null;
+
+
+    // Called when a collision happens involving this trigger
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        collision.transform.position = targetLocation.position;
+    }
+}
