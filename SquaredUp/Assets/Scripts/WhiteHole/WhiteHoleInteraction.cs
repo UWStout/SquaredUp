e6cdcb7d1558231cd7da064d04c941d17a031a0a
iz3k93 <isaacmoua93@gmail.com>
2021-04-28 17:07:13 -0500

    Ending???

---
+++ SquaredUp/Assets/Scripts/WhiteHole/WhiteHoleInteraction.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhiteHoleInteraction : Interactable
+{
+    // Point of rotation
+    [SerializeField] GameObject rotatingCenter = null;
+    [SerializeField] GameObject player = null;
+    [SerializeField] GameObject whiteHole = null;
+
+
+    /// <summary>Starts a dialogue with the NPC.</summary>
+    public override void Interact()
+    {
+        whiteHole.transform.GetChild(1).gameObject.SetActive(false);
+        player.transform.position = Vector3.MoveTowards(player.transform.position, whiteHole.transform.position, 1);
+    }
+
+}
