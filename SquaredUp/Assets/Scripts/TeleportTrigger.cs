d0ce6a7082b4a8551da358ca6cc64ab78103d0ea
SenalikW0106 <wsenalik@hotmail.com>
2021-04-28 14:20:18 -0500

    Fixes to king
    Teleport works correctly now.
    
    His vision is now visible.
    
    Changed his text

--- SquaredUp/Assets/Scripts/TeleportTrigger.cs
+++ SquaredUp/Assets/Scripts/TeleportTrigger.cs
@@ -5,11 +5,22 @@ public class TeleportTrigger : MonoBehaviour
 {
     // The transform of the location to teleport to
     [SerializeField] private Transform targetLocation = null;
+    [SerializeField] private bool changeX = true;
+    [SerializeField] private bool changeY = false;
 
 
     // Called when a collision happens involving this trigger
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        collision.transform.position = targetLocation.position;
+        Vector3 pos = collision.gameObject.transform.position;
+        if (changeX)
+        {
+            pos.x = targetLocation.position.x;
+        }
+        if (changeY)
+        {
+            pos.y = targetLocation.position.y;
+        }
+        collision.transform.position = pos;
     }
 }
