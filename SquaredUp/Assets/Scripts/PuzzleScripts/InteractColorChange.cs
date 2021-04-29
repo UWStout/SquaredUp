3bdb8e57e1181dfd66b6e06bc4c44a4a775f4fc5
SenalikW0106 <wsenalik@hotmail.com>
2021-04-29 05:32:38 -0500

    Final build stuff

--- SquaredUp/Assets/Scripts/PuzzleScripts/InteractColorChange.cs
+++ SquaredUp/Assets/Scripts/PuzzleScripts/InteractColorChange.cs
@@ -4,18 +4,16 @@ using UnityEngine;
 
 public class InteractColorChange : Interactable
 {
-    [SerializeField] private GameObject colorChangeObject;
     [SerializeField] private int color;
     //override interactable
     public override void Interact()
     {
-        //change character to blue
-        colorChangeObject.GetComponent<ChangeColorSkill>().Use(color);
+        // Change color to specified color
+        ChangeColorSkill.Instance.Use(color);
     }
 
     //set the object that you want to change the color of
     public void SetColorChangeObject(GameObject g)
     {
-        colorChangeObject = g;
     }
 }
