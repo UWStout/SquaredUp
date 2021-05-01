0e144bd664381ef4abcae31e3f9f18a2f3bfe3b3
TRKmanges <71797525+TRKmanges@users.noreply.github.com>
2021-05-01 16:11:57 -0500

    Ice fixes
    removed softlock
    fixed ice?

--- SquaredUp/Assets/Scripts/PuzzleScripts/PushableFix.cs
+++ SquaredUp/Assets/Scripts/PuzzleScripts/PushableFix.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public class PushableFix : MonoBehaviour
 {
-    [SerializeField] private GameObject parentObject;
+    [SerializeField] private Rigidbody2D parentPhys;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -11,18 +11,20 @@ public class PushableFix : MonoBehaviour
     }
     private void OnCollisionStay2D(Collision2D collision)
     {
-        function(collision);
+        //function(collision);
     }
 
     private void function(Collision2D collision)
     {
-        Vector2 hitPos = collision.GetContact(0).point;
         if (collision.gameObject.tag == "Player")
         {
+            parentPhys.constraints = RigidbodyConstraints2D.FreezePosition | RigidbodyConstraints2D.FreezeRotation;
+            Vector2 hitPos = (collision.GetContact(0).point + collision.GetContact(collision.contactCount - 1).point) / 2;
             Vector2 dir = new Vector2(0, 0);
             if (Mathf.Abs(transform.position.x - hitPos.x) > Mathf.Abs(transform.position.y - hitPos.y))
             {
                 dir.y = 0;
+                parentPhys.constraints = RigidbodyConstraints2D.FreezePositionY | ~RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezeRotation;
                 if (Mathf.Abs(transform.position.x) > Mathf.Abs(hitPos.x))
                 {
                     dir.x = 1;
@@ -35,6 +37,7 @@ public class PushableFix : MonoBehaviour
             else
             {
                 dir.x = 0;
+                parentPhys.constraints = ~RigidbodyConstraints2D.FreezePositionY | RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezeRotation;
                 if (Mathf.Abs(transform.position.y) > Mathf.Abs(hitPos.y))
                 {
                     dir.y = 1;
@@ -44,7 +47,7 @@ public class PushableFix : MonoBehaviour
                     dir.y = -1;
                 }
             }
-            parentObject.GetComponent<Rigidbody2D>().velocity = dir * 12;
+            parentPhys.velocity = dir * 12;
 
         }
     }
