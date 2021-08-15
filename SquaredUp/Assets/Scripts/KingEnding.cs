b0926241c8882cb6df1b7be769b4c6d2ab1d2341
Wyatt Senalik <wsenalik@hotmail.com>
2021-08-15 15:07:18 -0500

    Work on changing level 2 end

---
+++ SquaredUp/Assets/Scripts/KingEnding.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KingEnding : MonoBehaviour
+{
+    [SerializeField] private Follow camFollow = null;
+    [SerializeField] private Transform kingTrans = null;
+    [SerializeField] private Transform playerTrans = null;
+
+    [SerializeField] private float moveSpeed = 1.0f;
+    [SerializeField] private float moveTime = 5.0f;
+    [SerializeField] private Vector3 playerEndPos = Vector3.zero;
+
+    [SerializeField] private float fadeOutTime = 1.0f;
+    [SerializeField] private string noControlsMapName = "EndGame";
+
+
+    private Vector3 kingOriginalPosition = Vector3.zero;
+
+
+    public void StartEnding()
+    {
+        camFollow.StopFollow();
+        InputController.Instance.SwitchInputMap(noControlsMapName);
+        StartCoroutine(MoveCoroutine());
+    }
+
+
+    private IEnumerator MoveCoroutine()
+    {
+        kingOriginalPosition = kingTrans.position;
+
+        float t = 0;
+        while (t < moveTime)
+        {
+            kingTrans.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+            playerTrans.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+
+            t += Time.deltaTime;
+            yield return null;
+        }
+
+        CanvasSingleton.Instance.StartFadeOutAndIn(fadeOutTime, Teleport, RestoreInput);
+    }
+
+    private void Teleport()
+    {
+        kingTrans.position = kingOriginalPosition;
+        playerTrans.position = playerEndPos;
+        camFollow.StopFollow();
+    }
+
+    private void RestoreInput()
+    {
+        InputController.Instance.PopInputMap(noControlsMapName);
+    }
+}
---
+++ SquaredUp/Assets/Scripts/PlayAnimation.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Wrapper to call an animation from a unity event.
+/// </summary>
+[RequireComponent(typeof(Animation))]
+public class PlayAnimation : MonoBehaviour
+{
+    private Animation anim = null;
+
+
+    private void Awake()
+    {
+        anim = GetComponent<Animation>();
+    }
+
+
+    public void Play()
+    {
+        anim.Play();
+    }
+}
