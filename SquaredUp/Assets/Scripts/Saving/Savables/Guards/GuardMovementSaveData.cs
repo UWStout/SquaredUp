be7bcfe23454a69e73d93c251ee1a764b27fbc9a
Wyatt Senalik <wsenalik@hotmail.com>
2021-08-27 00:28:54 -0500

    Fixed saving issue and changed pushables

--- SquaredUp/Assets/Scripts/Saving/Savables/Guards/GuardMovementSaveData.cs
+++ SquaredUp/Assets/Scripts/Saving/Savables/Guards/GuardMovementSaveData.cs
@@ -7,15 +7,16 @@ using UnityEngine;
 [Serializable]
 public class GuardMovementSaveData : GuardMovementBaseSaveData
 {
-    public Vector2 GetLastMoveDirection() => lastMoveDir;
-    private Vector2 lastMoveDir = Vector2.zero;
+    public Vector2 GetLastMoveDirection() => new Vector2(lastMoveDir[0], lastMoveDir[1]);
+    private float[] lastMoveDir = new float[2];
     public bool GetIsFirstMove() => isFirstMove;
     private bool isFirstMove = true;
 
 
     public GuardMovementSaveData(GuardMovement guardMovement) : base(guardMovement)
     {
-        lastMoveDir = guardMovement.GetLastMoveDirection();
+        Vector2 lastMoveDirV2 = guardMovement.GetLastMoveDirection();
+        lastMoveDir = new float[2] { lastMoveDirV2.x, lastMoveDirV2.y };
         isFirstMove = guardMovement.CheckIsFirstMove();
     }
 }
