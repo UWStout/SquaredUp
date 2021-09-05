9355735164f2199d8902acf8440b1ec758b81c92
SenalikW0106 <wsenalik@hotmail.com>
2021-09-05 12:46:17 -0500

    Should have fixed the problem with the puzzle loader menu

--- SquaredUp/Assets/Scripts/PuzzleLoading/PlayerInitializer.cs
+++ SquaredUp/Assets/Scripts/PuzzleLoading/PlayerInitializer.cs
@@ -9,33 +9,36 @@ public class PlayerInitializer : MonoBehaviour
     {
         // Test to make sure there is a puzzle loader
         PuzzleLoader puzzleLoader = PuzzleLoader.Instance;
-        if (puzzleLoader != null)
+        if (puzzleLoader == null)
         {
-            // Test to see if we have a puzzle we are trying to load into
-            PuzzleLoadData data = puzzleLoader.GetPuzzleData();
-            if (data != null)
-            {
-                // Move the player
-                Vector3 pos = data.StartPosition;
-                if (pos != Vector3.negativeInfinity)
-                {
-                    PlayerMovement.Instance.transform.position = pos;
-                }
+            return;
+        }
+        // Test to see if we have a puzzle we are trying to load into
+        PuzzleLoadData data = puzzleLoader.GetPuzzleData();
+        if (data == null)
+        {
+            return;
+        }
 
-                // Unlock their shape states
-                int[] shapeUnlockStates = data.ShapeUnlockStates;
-                foreach (int state in shapeUnlockStates)
-                {
-                    SkillController.Instance.UnlockSkillState(SkillController.SkillEnum.Shape, state);
-                }
+        // Move the player
+        Vector3 pos = data.StartPosition;
+        if (pos != Vector3.negativeInfinity)
+        {
+            PlayerMovement.Instance.GetComponent<GridMover>().SetPosition(pos);
+        }
 
-                // Unlock their color states
-                int[] colorUnlockStates = data.ColorUnlockStates;
-                foreach (int state in colorUnlockStates)
-                {
-                    SkillController.Instance.UnlockSkillState(SkillController.SkillEnum.Color, state);
-                }
-            }
+        // Unlock their shape states
+        int[] shapeUnlockStates = data.ShapeUnlockStates;
+        foreach (int state in shapeUnlockStates)
+        {
+            SkillController.Instance.UnlockSkillState(SkillController.SkillEnum.Shape, state);
+        }
+
+        // Unlock their color states
+        int[] colorUnlockStates = data.ColorUnlockStates;
+        foreach (int state in colorUnlockStates)
+        {
+            SkillController.Instance.UnlockSkillState(SkillController.SkillEnum.Color, state);
         }
     }
 }
