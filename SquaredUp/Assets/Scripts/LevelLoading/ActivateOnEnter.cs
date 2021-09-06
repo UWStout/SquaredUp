51d1505f91f138446f954eca607982879ead1107
SenalikW0106 <wsenalik@hotmail.com>
2021-09-06 14:07:00 -0500

    Level loading/unloading for level 2
    Need to turn off level 2 yet

---
+++ SquaredUp/Assets/Scripts/LevelLoading/ActivateOnEnter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class ActivateOnEnter : MonoBehaviour
+{
+    [SerializeField] private GameObject[] activateObjs = null;
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            foreach (GameObject go in activateObjs)
+            {
+                go.SetActive(true);
+            }
+        }
+    }
+}
---
+++ SquaredUp/Assets/Scripts/LevelLoading/DeactivateOnExit.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class DeactivateOnExit : MonoBehaviour
+{
+    [SerializeField] private GameObject[] deactivateObjs = null;
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            foreach (GameObject go in deactivateObjs)
+            {
+                go.SetActive(false);
+            }
+        }
+    }
+}
