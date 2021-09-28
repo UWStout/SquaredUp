01e5b2ac5e636763a3edfd53e015f9fb518827e5
SenalikW0106 <wsenalik@hotmail.com>
2021-09-28 17:24:11 -0500

    Credits

---
+++ SquaredUp/Assets/Scripts/AutoMove.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class AutoMove : MonoBehaviour
+{
+    [SerializeField] private Vector3 _movementSpeed = Vector3.zero;
+
+
+    private void Update()
+    {
+        transform.position += _movementSpeed * Time.deltaTime;
+    }
+}
---
+++ SquaredUp/Assets/Scripts/PositionResetter.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class PositionResetter : MonoBehaviour
+{
+    private Vector3 _originalPosition = Vector3.zero;
+
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _originalPosition = transform.position;
+    }
+
+
+    public void ResetPosition()
+    {
+        transform.position = _originalPosition;
+    }
+}
